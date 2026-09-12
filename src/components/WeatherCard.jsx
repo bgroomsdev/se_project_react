6622c14b3e715ddc49b2d6e974506cacfc703b98
bradengrooms <bgroo2004@gmail.com>
2026-09-12 16:31:35 -0500

    fix weather images using public folder and BASE_URL

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,30 +1,37 @@
 import "../blocks/weather-card.css";
-import sunnyDay from "../assets/sunnyDay.svg";
-import cloudyDay from "../assets/cloudyDay.svg";
-import rainyDay from "../assets/rainyDay.svg";
-import stormyDay from "../assets/stormyDay.svg";
-import snowyDay from "../assets/snowyDay.svg";
-import foggyDay from "../assets/foggyDay.svg";
-import sunnyNight from "../assets/sunnyNight.svg";
-import cloudyNight from "../assets/cloudyNight.svg";
-import rainyNight from "../assets/rainyNight.svg";
-import stormyNight from "../assets/stormyNight.svg";
-import snowyNight from "../assets/snowyNight.svg";
-import foggyNight from "../assets/foggyNight.svg";
 import { useContext } from "react";
 import { CurrentTemperatureUnitContext } from "../contexts/CurrentTemperatureUnitContext";
 
-const weatherImages = {
-  sunny: { day: sunnyDay, night: sunnyNight },
-  cloudy: { day: cloudyDay, night: cloudyNight },
-  rainy: { day: rainyDay, night: rainyNight },
-  stormy: { day: stormyDay, night: stormyNight },
-  snowy: { day: snowyDay, night: snowyNight },
-  foggy: { day: foggyDay, night: foggyNight },
-};
-
 function WeatherCard({ weatherData }) {
   const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
+
+  const weatherImages = {
+    sunny: {
+      day: `${import.meta.env.BASE_URL}sunnyDay.svg`,
+      night: `${import.meta.env.BASE_URL}sunnyNight.svg`,
+    },
+    cloudy: {
+      day: `${import.meta.env.BASE_URL}cloudyDay.svg`,
+      night: `${import.meta.env.BASE_URL}cloudyNight.svg`,
+    },
+    rainy: {
+      day: `${import.meta.env.BASE_URL}rainyDay.svg`,
+      night: `${import.meta.env.BASE_URL}rainyNight.svg`,
+    },
+    stormy: {
+      day: `${import.meta.env.BASE_URL}stormyDay.svg`,
+      night: `${import.meta.env.BASE_URL}stormyNight.svg`,
+    },
+    snowy: {
+      day: `${import.meta.env.BASE_URL}snowyDay.svg`,
+      night: `${import.meta.env.BASE_URL}snowyNight.svg`,
+    },
+    foggy: {
+      day: `${import.meta.env.BASE_URL}foggyDay.svg`,
+      night: `${import.meta.env.BASE_URL}foggyNight.svg`,
+    },
+  };
+
   const timeOfDay = weatherData.isDay ? "day" : "night";
   const image = weatherImages[weatherData.weatherType]?.[timeOfDay];
 
